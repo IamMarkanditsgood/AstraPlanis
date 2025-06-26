dd8ad720e8bbc37b77b1231564619ddd1f3883d5
PavloMarkovskiy <IamMarkanditsgood@users.noreply.github.com>
2025-06-26 10:08:23 +0200

    Privacy

--- Assets/Scripts/UI/Screens/Variables/Info.cs
+++ Assets/Scripts/UI/Screens/Variables/Info.cs
@@ -8,18 +8,19 @@ public class Info : BasicScreen
 {
     [SerializeField] private Button _profileButton;
     [SerializeField] private Button _homeButton;
+    [SerializeField] private Button _privacyButton;
 
     private void Start()
     {
         _profileButton.onClick.AddListener(ProfileButton);
         _homeButton.onClick.AddListener(HomeButton);
-
+        _privacyButton.onClick.AddListener(Privacy);
     }
     private void OnDestroy()
     {
         _profileButton.onClick.RemoveListener(ProfileButton);
         _homeButton.onClick.RemoveListener(HomeButton);
-
+        _privacyButton.onClick.RemoveListener(Privacy);
     }
 
     public override void ResetScreen()
@@ -38,5 +39,8 @@ public class Info : BasicScreen
     {
         UIManager.Instance.ShowScreen(ScreenTypes.Home);
     }
-
+    private void Privacy()
+    {
+        Application.OpenURL("https://www.privacypolicies.com/live/dd4b6f5e-9464-47f2-98ad-54f7b8e2ed5a");
+    }
 }
